78e034fb7f115747b0b71f37b596e21478cc1036
Mauricio Pruss Duarte <prussdev@gmail.com>
2020-08-14 21:09:05 -0300

    Subindo alteracoes

---
+++ src/Views/ListSchemas.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+  import SchemaCard from './components/SchemaCard';
+  import SearchBar from './components/SearchBar';
+  import NavBar from './components/NavBar';
+
+  export default {
+    name: 'ListSchemas',
+    components: {
+      SchemaCard,
+      SearchBar,
+      NavBar
+    }
+  }
+</script>
+
+<style>
+
+</style>
